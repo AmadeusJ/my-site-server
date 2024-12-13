5543b74562e4475dd5ec828dd2bb1faa0c7a7832
Amadeus J <dawoonjung1026@gmail.com>
2024-12-13 15:38:47 +0900

    email 테스트 완료

--- app/mailer.py
+++ app/mailer.py
@@ -6,7 +6,7 @@ import os
 SMTP_SERVER = os.getenv("SMTP_SERVER")
 SMTP_PORT = int(os.getenv("SMTP_PORT"))
 SMTP_USER = os.getenv("SMTP_USER")
-SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
+SMTP_PASSWORD = os.getenv("SMTP_PASSWORD").strip()
 
 def send_email(to_email: str, subject: str, body: str) -> bool:
 
