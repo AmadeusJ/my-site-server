311c34f7c01f9005c5e91d45876ffd092e6903fb
Amadeus J <dawoonjung1026@gmail.com>
2024-12-16 14:14:40 +0900

    hahaha

--- app/logger.py
+++ app/logger.py
@@ -1,4 +1,20 @@
 import logging
+from logging.handlers import RotatingFileHandler
+import os
+
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+
+log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+info_handler = RotatingFileHandler(f"{log_dir}/info.log", maxBytes=1000000, backupCount=5)
+info_handler.setLevel(logging.INFO)
+info_handler.setFormatter(logging.Formatter(log_format))
+
+error_handler = RotatingFileHandler(f"{log_dir}/error.log", maxBytes=1000000, backupCount=5)
+error_handler.setLevel(logging.ERROR)
+error_handler.setFormatter(logging.Formatter(log_format))
+
 
 # 로거 설정
 logger = logging.getLogger("portfolio_api")
@@ -9,8 +25,10 @@ console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG)
 
 # 포맷터 설정
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+formatter = logging.Formatter(log_format)
 console_handler.setFormatter(formatter)
 
 # 핸들러 추가
 logger.addHandler(console_handler)
+logger.addHandler(info_handler)
+logger.addHandler(error_handler)
